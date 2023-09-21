4b180e13078939c16b01c55944c31f4f076528ab
BeatrizCano <beatrizcafernandez@gmail.com>
2023-09-21 10:28:36 +0200

    add_accent_mark_fenix

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Banco de Libros Fenix</title>
+        <title>Banco de Libros Fénix</title>
         <link rel="icon" href="assets/icon.png" />
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
@@ -17,13 +17,13 @@
         <header>
             <h1 class="site-heading text-center text-faded d-none d-lg-block">
                 <span class="site-heading-upper text-primary mb-3">Transmite tu conocimiento</span>
-                <span class="site-heading-lower">Banco de Libros Fenix</span>
+                <span class="site-heading-lower">Banco de Libros Fénix</span>
             </h1>
         </header>
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
             <div class="container">
-                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Banco de Libros Fenix</a>
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Banco de Libros Fénix</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto">
@@ -70,14 +70,14 @@
                                 <span class="section-heading-upper">Nuestro compromiso</span>
                                 <span class="section-heading-lower">Para ti</span>
                             </h2>
-                            <p class="mb-0">El Banco de Libros Fenix se ha creado con el objetivo de llevar la cultura a todos los rincones del mundo de manera gratuita. Nuestros equipo colabora de manera desinteresada en esta labor para que este ciclo nunca se detenga. Aprovechamos los recursos que ya existen para cumplir con nuestra misión. ¡Únete a nosotr@s!</p>
+                            <p class="mb-0">El Banco de Libros Fénix se ha creado con el objetivo de llevar la cultura a todos los rincones del mundo de manera gratuita. Nuestros equipo colabora de manera desinteresada en esta labor para que este ciclo nunca se detenga. Aprovechamos los recursos que ya existen para cumplir con nuestra misión. ¡Únete a nosotr@s!</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <footer class="footer text-faded text-center py-5">
-            <div class="container"><p class="m-0 small">Copyright &copy; Banco de Libros Fenix 2023</p></div>
+            <div class="container"><p class="m-0 small">Copyright &copy; Banco de Libros Fénix 2023</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
